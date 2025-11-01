d8ab003cd1407913cb1e62ecc380e1a3addc535b
KAMESH <kameshkamesh3876@gmail.com>
2025-11-01 14:03:40 +0530

    add server url

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,22 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
+
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:5000',
+export default ({ mode }) => {
+  // Load environment variables from .env file
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return defineConfig({
+    plugins: [react(), tailwindcss()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_BACKEND_URL, // using env variable
+          changeOrigin: true,
+          secure: false,
+        },
+      },
     },
-  },
-});
+  });
+};
